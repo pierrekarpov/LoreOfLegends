7efd68fafb0238285ac3714a76cad561adb1827f
Pierre Karpov <push.default>
2018-05-12 01:26:35 +0800

    get game info

--- test.php
+++ test.php
@@ -64,4 +64,32 @@ $game_ids = array();
 foreach ($json3["matches"] as $k => $v) {
   array_push($game_ids, $v["gameId"]);
 }
+
+$total = count($game_ids);
+
+foreach ($game_ids as $gid) {
+  $action = "GET";
+  $match_list_url = "https://na1.api.riotgames.com/lol/match/v3/matches/" . $gid;
+  $parameters = array(
+    "api_key" => $key
+  );
+  $json4 = CurlHelper::perform_http_request($action, $match_list_url, $parameters);
+  print $json4;
+  $json4 = json_decode($json4, true);
+
+  print $json4["participantIdentities"];
+  foreach ($json4["participantIdentities"] as $pa) {
+    foreach ($pa as $p => $pk) {
+      print $p . $pk["accountId"] . "\n";
+      if ($pk["accountId"] == $account_id) {
+
+      }
+    }
+  }
+  // foreach ($json4["stats"]["participantIdentities"] as $key => $value) {
+  //   print $key . $value;
+  //   // code...
+  // }
+}
+
 print "\n";
